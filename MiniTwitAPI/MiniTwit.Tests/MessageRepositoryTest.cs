1c15a935ff5b7c22764b35464875ec7747c81478
hefr2019 <hefr@itu.dk>
2021-03-26 11:13:44 +0100

    add get message test

--- MiniTwitAPI/MiniTwit.Tests/MessageRepositoryTest.cs
+++ MiniTwitAPI/MiniTwit.Tests/MessageRepositoryTest.cs
@@ -16,19 +16,19 @@ namespace MiniTwit.Tests
     public class MessageRepositoryTest : IDisposable
     {
         private readonly MiniTwitContext context;
-        private readonly UserRepository repo;
+        private readonly MessageRepository repo;
 
         public MessageRepositoryTest()
         {
             var builder = new DbContextOptionsBuilder<MiniTwitContext>();
-            builder.UseInMemoryDatabase(databaseName: "MiniTwitDatabase");   //.UseSqlite("datasource=:memory:");
+            builder.UseInMemoryDatabase(databaseName: "MiniTwitDatabase");
 
             var dbContextOptions = builder.Options;
             context = new ContextTest(dbContextOptions);
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
-            repo = new UserRepository(context);
+            repo = new MessageRepository(context);
         }
 
         public void Dispose()
@@ -37,15 +37,83 @@ namespace MiniTwit.Tests
         }
 
         [Fact]
-        public async Task unfollow_existing_user_being_followed()
+        public async Task create_and_message_existing_user_get_id()
         {
-            var followeduser = await repo.UnfollowUser("olduser2", "olduser1");
-            var followinguser = await repo.UnfollowUser("olduser1", "olduser2");
-
-            //Assert.Equal(0, result);
-            Assert.False(await repo.IsFollowing("olduser1", "olduser2"));
-            Assert.False(await repo.IsFollowing("olduser2", "olduser1"));
+            var result = await repo.AddMessage(
+                                                new MessageCreateDTO {
+                                                    content = "this is a new post"
+                                                }, 
+                                                "olduser1");
+            int messageId = 1;
+         
+            Assert.Equal(result, messageId);
         }
 
+        [Fact]
+        public async Task create_and_get_added_message()
+        {
+            var result = await repo.AddMessage(
+                                                new MessageCreateDTO
+                                                {
+                                                    content = "this is a new post"
+                                                },
+                                                "olduser1");
+            int messageId = 1;
+            var getMessage = await repo.GetMessage(messageId);
+
+            Assert.Equal(result, messageId);
+            Assert.Equal(messageId, getMessage.MessageId);
+        }
+
+        /*
+        [Fact]
+        public async Task create_message_non_existing_user()
+        {
+            var result = await repo.CreateAsync("testtext", "nonuser");
+
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public async Task read_nonexisting_message()
+        {
+            var result = await repo.ReadAsync(1337);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task delete_existing_message()
+        {
+            var messageId = await repo.CreateAsync("testtext", "olduser2");
+            var messagebefore = await repo.ReadAsync(messageId);
+            var result = await repo.DeleteAsync(messageId);
+            var messageafter = await repo.ReadAsync(messageId);
+
+            Assert.NotNull(messagebefore);
+            Assert.Null(messageafter);
+            Assert.Equal(messageId, result);
+        }
+
+        [Fact]
+        public async Task delete_nonexisting_message()
+        {
+            var result = await repo.DeleteAsync(1337);
+
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public async Task readall_messages()
+        {
+            await repo.CreateAsync("testtext1", "olduser1");
+            var first = await repo.ReadAllAsync();
+            await repo.CreateAsync("testtext2", "olduser1");
+            var second = await repo.ReadAllAsync();
+
+            Assert.Equal(2, first.Count);
+            Assert.Equal(3, second.Count);
+        }
+        */
     }
 }
