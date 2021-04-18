0fd5fc333272b27b9d177354ee9f7151a50896ec
hefr2019 <hefr@itu.dk>
2021-04-18 16:47:09 +0200

    cleanup entities

--- MiniTwitAPI/MiniTwit.Entities/MiniTwitContext.cs
+++ MiniTwitAPI/MiniTwit.Entities/MiniTwitContext.cs
@@ -6,9 +6,9 @@ namespace MiniTwit.Entities
 {
     public partial class MiniTwitContext : DbContext, IMiniTwitContext
     {
-        public virtual DbSet<Follower> Followers { get; set; }
-        public virtual DbSet<Message> Messages { get; set; }
-        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Follower> Followers {get; set;}
+        public virtual DbSet<Message> Messages {get; set;}
+        public virtual DbSet<User> Users {get; set;}
 
         public MiniTwitContext(DbContextOptions<MiniTwitContext> options) : base(options)
         {
@@ -18,7 +18,7 @@ namespace MiniTwit.Entities
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Follower>()
-                                            .HasKey(f => new { f.WhoId, f.WhomId });
+                                           .HasKey(f => new {f.WhoId, f.WhomId});
         }
     }
 }
